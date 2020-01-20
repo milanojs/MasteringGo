9e81fd83f81200159336e11306d90ecaa32d924e
Juan Milano <juan_milano@hotmail.com>
2020-01-20 11:03:35 -0400

    revision steps

--- chapter02/unsafe.go
+++ chapter02/unsafe.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"unsafe"
 )
 
 func main() {
-	fmt.Println("Hello World, this is unsafe!")
+	var value int64 = 5
+	var p1 = &value
+	var p2 = (*int32)(unsafe.Pointer(p1))
+	fmt.Println("*p1: ", *p1)
+	fmt.Println("*p2: ", *p2)
+	*p1 = 5434123412312431212
+	fmt.Println(value)
+	fmt.Println("*p2: ", *p2)
+	*p1 = 54341234
+	fmt.Println(value)
+	fmt.Println("*p2: ", *p2)
 }
